c912d905d430a96b30388e15267222c0302d98c5
MacBook Pro <naym100m@gmail.com>
2024-06-23 10:43:44 +0600

    cmpleted a [UserModel] use mongoose and typeScript

--- src/model/User.ts
+++ src/model/User.ts
@@ -20,10 +20,57 @@ const MessageSchema: Schema<Message> = new Schema({
 
 // Users Info Schema
 export interface User extends Document {
+  username: string;
   email: string;
   password: string;
   verifyCode: string;
-  verifyCodeExpiry: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
   isAcceptingMessage: boolean;
-  message: Message[];
+  messages: Message[];
 }
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+\@.+\..+/, "please use valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    unique: true,
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "Verify Code is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify code expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: {
+    type: [MessageSchema],
+  },
+});
+
+const userModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default userModel;
