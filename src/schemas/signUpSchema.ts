71d6159049f863f63888dc45de1bb3eca30c99a3
MacBook Pro <naym100m@gmail.com>
2024-06-23 14:49:26 +0600

    create a next label schemas in user signup and messages velidations

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -5,3 +5,11 @@ export const usernameValidation = z
   .min(2, "Username must be at least 2 characters")
   .max(20, "Username must be no more then 20 characters")
   .regex(/^[a-zA-Z0-9_]+$/, "Username not use special characters");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "password must be at least 6 characters" }),
+});
