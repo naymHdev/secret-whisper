5ee5cbaf8e57ca40b563e3c9d7b48704a7c045da
MacBook Pro <naym100m@gmail.com>
2024-07-01 21:03:54 +0600

    created: middleware creating for routing user signin and signup

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (token) {
+    // If the user is authenticated and tries to access sign-in or sign-up pages, redirect them to the dashboard
+    if (
+      url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/"
+    ) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  } else {
+    // If the user is not authenticated, redirect them to the home page for other routes
+    if (!url.pathname.startsWith("/")) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  // Allow the request to proceed for other paths or authenticated users accessing the dashboard
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
+};
