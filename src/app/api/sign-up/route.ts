1456486c4e85d721232b1542ab046969e1f864c1
MacBook Pro <naym100m@gmail.com>
2024-07-01 17:37:19 +0600

    added: create and added some user signup validation

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -8,6 +8,29 @@ export async function POST(request: Request) {
 
   try {
     const { username, email, password } = await request.json();
+
+    const existingUserVerifiedByUsername = await userModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return (
+        Response.json({
+          success: false,
+          message: "Username is already taken",
+        }),
+        { status: 400 }
+      );
+    }
+
+    const existingUserByEmail = await userModel.findOne({ email });
+    if (existingUserByEmail) {
+      true; // TODO: Back hare
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const expiryDate = new Date();
+      expiryDate.setHours(expiryDate.getHours() + 1);
+    }
   } catch (error) {
     console.error("Error registering user", error);
     return Response.json(
