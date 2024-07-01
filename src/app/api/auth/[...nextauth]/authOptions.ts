956c52cc9a5e9e9bf03fe7e9c859c13eb575d65e
MacBook Pro <naym100m@gmail.com>
2024-07-01 20:26:21 +0600

    created: authoption user login seasion ant token verified

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
 import dbConnect from "@/lib/dbConnect";
@@ -28,10 +29,51 @@ export const authOptions: NextAuthOptions = {
           if (!user) {
             throw new Error("No user found this email");
           }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account first!");
+          }
+          const isPasswordConnect = await bcrypt.compare(
+            credentials.Password,
+            user.password
+          );
+          if (isPasswordConnect) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
         } catch (error: any) {
           throw new Error(error);
         }
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user?._id?.toString();
+        token.isverified = user?.isverified;
+        token.isAcceptingMessage = user?.isAcceptingMessage;
+        token.username = user?.username;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isverified = token.isverified;
+        session.user.isAcceptingMessage = token.isAcceptingMessage;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_SECRET,
 };
