8f6b6e43f7a57da324c7ad331ef7b044cda27720
MacBook Pro <naym100m@gmail.com>
2024-07-16 22:51:12 +0600

    [created a] username verification model

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -11,7 +11,54 @@ const UserNameSchema = z.object({
 export async function GET(request: Request) {
   await dbConnect();
   try {
+    const { searchParams } = new URL(request.url);
+    const queryParams = {
+      username: searchParams.get("username"),
+    };
+    // validate with Zod
+    const result = UserNameSchema.safeParse(queryParams);
+    console.log("result___", result);
+    if (!result.success) {
+      const usernameErrors = result.error.format().username?._errors || [];
+      return Response.json({
+        success: false,
+        message:
+          usernameErrors?.length > 0
+            ? usernameErrors.join(", ")
+            : "Invalid query parameters",
+      });
+    }
+
+    const { username } = result.data;
+
+    const existingVerifiedUser = await userModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingVerifiedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
+        },
+        { status: 500 }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Username is unique",
+      },
+      { status: 400 }
+    );
   } catch (error) {
     console.error("Error checking username_", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error checking username",
+      },
+      { status: 500 }
+    );
   }
 }
