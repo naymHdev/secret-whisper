0a080609d3d160727ea068a65cdd0d676b119db9
MacBook Pro <naym100m@gmail.com>
2024-07-17 11:47:33 +0600

    [created a] post method use mongoose for user updated request

--- src/app/accept-messages/route.ts
+++ src/app/accept-messages/route.ts
@@ -3,3 +3,66 @@ import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 import dbConnect from "@/lib/dbConnect";
 import userModel from "@/model/User";
 import { User } from "next-auth";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+  //   const user = session?.user;
+
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await userModel.findByIdAndUpdate(
+      userId,
+      {
+        isAcceptingMessage: acceptMessages,
+      },
+      { new: true }
+    );
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status to accept messages",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: "Message acceptance status updated successfully",
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Failed to update user status to accept messages", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status to accept messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
