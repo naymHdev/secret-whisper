9f78e945de9c8f874392150f58380ffc516aaf95
MacBook Pro <naym100m@gmail.com>
2024-07-26 21:36:34 +0600

    [compleated ] a user get messages with mongodb aggregation

--- src/app/api/get-messages/route.ts
+++ src/app/api/get-messages/route.ts
@@ -1,12 +1,13 @@
 import dbConnect from "@/lib/dbConnect";
 import { getServerSession, User } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/authOptions";
+import mongoose from "mongoose";
+import userModel from "@/model/User";
 
 export async function GET(request: Request) {
   await dbConnect();
   const session = await getServerSession(authOptions);
   const user: User = session?.user as User;
-  //   const user = session?.user;
 
   if (!session || !session.user) {
     return Response.json(
@@ -20,5 +21,35 @@ export async function GET(request: Request) {
     );
   }
 
-  const userId = user._id;
+  const userId = new mongoose.Types.ObjectId(user._id);
+
+  try {
+    const user = await userModel.aggregate([
+      { $match: { id: userId } },
+      { $unwind: "$messages" },
+      { $sort: { "messages.createdAt": -1 } },
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not-found",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+    return Response.json(
+      {
+        success: true,
+        message: user[0].messages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {}
 }
