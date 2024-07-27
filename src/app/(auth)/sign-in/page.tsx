8f966a169333f919f5bbc95715391bc9d10b08df
MacBook Pro <naym100m@gmail.com>
2024-07-27 19:29:18 +0600

    [compleate a] user dynami verify code route

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,83 @@
 "use client";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { signInSchema } from "@/schemas/signinSchema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
 
 const SignIn = () => {
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    try {
+    } catch (error) {}
+  };
+
   return (
     <>
-      <div>
-        
+      <div className=" flex flex-col items-center justify-center h-screen">
+        <div className="max-w-7xl w-full lg:w-5/12 mx-auto px-4 md:px-6 lg:px-10 shadow-md bg-slate-100 py-10 rounded">
+          <div className=" text-center py-8">
+            <h1 className=" text-4xl font-extrabold">Sign In Your Account</h1>
+            <p className="mt-3 font-medium">
+              Sign in to start your antonymous adventure
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="exapmle@gmail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="******" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Sign in</Button>
+            </form>
+          </Form>
+          <div className=" mt-5 text-center">
+            <p>
+              Are you do not create a account?
+              <Link
+                href="/sign-up"
+                className=" text-blue-600 hover:text-blue-800"
+              >
+                <span> Sign up now.</span>
+              </Link>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { useToast } from "@/components/ui/use-toast";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { useParams, useRouter } from "next/navigation";
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const VerifyAccount = () => {
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    try {
+      const res = await axios.post(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+
+      toast({
+        title: "Success",
+        description: res?.data?.message,
+      });
+
+      router.replace("sign-in");
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Code verify failed!",
+        description: axiosError.response?.data.message,
+        variant: "destructive",
+      });
+    }
+  };
+
+  return (
+    <>
+      <div className=" flex flex-col items-center justify-center h-screen">
+        <div className="max-w-7xl w-full lg:w-5/12 mx-auto px-4 md:px-6 lg:px-10 shadow-md bg-slate-100 py-10 rounded">
+          <div className=" text-center py-8">
+            <h1 className=" text-4xl font-extrabold">Verify Your Account</h1>
+            <p className="mt-3 font-medium">
+              Enter the verification code sent to your email.
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="code"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Verification Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder="submit your code" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VerifyAccount;
