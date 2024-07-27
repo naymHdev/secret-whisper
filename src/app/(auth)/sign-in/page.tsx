3727b8e01445a4c1d8f497e5bee64eb04b413843
MacBook Pro <naym100m@gmail.com>
2024-07-27 10:09:47 +0600

    compleated || a user sign in all functionality

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -8,8 +8,28 @@ import { useDebounceValue } from "usehooks-ts";
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import { signUpSchema } from "@/schemas/signUpSchema";
+import { useEffect, useState } from "react";
+import axios, { isCancel, AxiosError } from "axios";
+import { ApiResponse } from "@/types/ApiResponse";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 
 const SignIn = () => {
+  const [username, setUsername] = useState("");
+  const [usernameMessages, setUsernameMessages] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debouncedUsername = useDebounceValue(username, 300);
+
   const { toast } = useToast();
   const router = useRouter();
 
@@ -23,9 +43,84 @@ const SignIn = () => {
     },
   });
 
+  // Username checking
+  useEffect(() => {
+    const checkUsernameUnique = async () => {
+      if (debouncedUsername) {
+        setIsCheckingUsername(true);
+        setUsernameMessages("");
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debouncedUsername}`
+          );
+          // console.log("checkUsernameUnique response >", response);
+          setUsernameMessages(response.data.message);
+        } catch (error) {
+          console.error("User name messages error", error);
+
+          const axiosError = error as AxiosError<ApiResponse>;
+          setUsernameMessages(
+            axiosError.response?.data.message ?? "Error checking user name"
+          );
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+
+    checkUsernameUnique();
+  }, [debouncedUsername]);
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      const res = await axios.post<ApiResponse>("/api/sign-up", data);
+      // console.log("User sign up response data", res.data);
+      toast({
+        title: "Sign up successfully completed",
+        description: res.data.message,
+      });
+      router.replace(`/verify/${username}`);
+    } catch (error) {
+      console.error("User name messages error", error);
+
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title: "Sign up failed!",
+        description: axiosError.response?.data.message,
+        variant: "destructive",
+      });
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <>
-      <div></div>
+      <div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </div>
     </>
   );
 };
