7d67d9d4a0f9f96011fd1c39e2d4cab2f7a9b42e
MacBook Pro <naym100m@gmail.com>
2024-07-27 09:02:41 +0600

    [updated and modified] a sign in page with react hook form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,33 +1,32 @@
 "use client";
-import { signIn, signOut, useSession } from "next-auth/react";
-import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useDebounceValue } from "usehooks-ts";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import { signUpSchema } from "@/schemas/signUpSchema";
 
 const SignIn = () => {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button
-          className="btn border px-6 py-2 rounded-md bg-orange-400"
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </>
-    );
-  }
+  const { toast } = useToast();
+  const router = useRouter();
+
+  // Define a form with Zod
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
   return (
-    <div className=" flex flex-col h-screen justify-center items-center">
-      Not signed in <br />
-      <button
-        className="btn border px-6 py-2 rounded-md bg-orange-400 mt-2 text-white font-semibold"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </div>
+    <>
+      <div></div>
+    </>
   );
 };
 
