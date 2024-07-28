09b3921a97f3858e650adeb99d36e475a6918f31
MacBook Pro <naym100m@gmail.com>
2024-07-28 21:36:01 +0600

    [added ] a autoplay in shadcn carousel

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 const Home = () => {
   return (
     <>
@@ -9,6 +19,28 @@ const Home = () => {
             Dive into the World of Anonymous Conversations
           </h1>
         </section>
+        <Carousel
+          plugins={[Autoplay({ delay: 2000 })]}
+          className="w-full max-w-xs"
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </main>
     </>
   );
