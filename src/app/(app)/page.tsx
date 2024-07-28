f6baae445b27b7941306e78014da57cef513f6e6
MacBook Pro <naym100m@gmail.com>
2024-07-28 21:29:50 +0600

    fixed dashboard page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 
-
-export default function Home() {
+const Home = () => {
   return (
-    <main className="h-screen flex items-center justify-center">
-      <h1 className=" text-5xl font-extrabold">Secret Whisper</h1>
-    </main>
+    <>
+      <main className=" max-w-7xl mx-auto px-4 md:px-6 lg:px-10 py-12 flex-grow flex flex-col items-center justify-center">
+        <section className=" text-center mb-8 md:mb-12">
+          <h1 className=" text-4xl font-bold">
+            Dive into the World of Anonymous Conversations
+          </h1>
+        </section>
+      </main>
+    </>
   );
-}
+};
+
+export default Home;
---
+++ src/data/data.ts
@@ -0,0 +1,25 @@
+export const staticMessage = [
+  {
+    title: "Welcome to the Platform!",
+    content: "Thank you for joining us. We are excited to have you on board.",
+    received: "10 minutes ago",
+  },
+  {
+    title: "Important Update",
+    content:
+      "We have updated our terms of service. Please review the changes at your earliest convenience.",
+    received: "2 minutes ago",
+  },
+  {
+    title: "Monthly Newsletter",
+    content:
+      "Check out our latest newsletter for updates on new features and upcoming events.",
+    received: "6 minutes ago",
+  },
+  {
+    title: "Reminder: Upcoming Event",
+    content:
+      "Don't forget about our webinar on July 30th. Register now to secure your spot!",
+    received: "30 minutes ago",
+  },
+];
