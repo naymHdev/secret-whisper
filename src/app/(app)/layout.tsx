fb71b704453127d8ea59adb6d95b0b01a9a66bea
MacBook Pro <naym100m@gmail.com>
2024-07-30 18:56:50 +0600

    [created a ] footer section

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className=" flex flex-col items-center justify-center py-8 bg-slate-700 text-slate-50">
+          <h3 className="' font-medium">
+            © 2023 Secret Feedback. All rights reserved.
+          </h3>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
